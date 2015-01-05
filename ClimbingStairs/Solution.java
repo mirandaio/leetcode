9c3d11b0fea5b04f852352b82262559cbb7d72f0
Alfredo Miranda <alfredo@miranda.io>
2015-01-04 18:51:49 -0600

    Complete Climbing Stairs using constant memory, bottom up approach

--- ClimbingStairs/Solution.java
+++ ClimbingStairs/Solution.java
@@ -1,24 +1,18 @@
 public class Solution {
     public int climbStairs(int n) {
-        int[] memo = new int[n + 1];
-        return climbStairsHelper(n, memo);
-    }
-
-    private int climbStairsHelper(int n, int[] memo) {
-        if(memo[n] != 0)
-            return memo[n];
-
-        if(n < 0)
-            return 0;
-
-        int f;
-            
         if(n <= 1)
-            f = 1;
-        else
-            f = climbStairsHelper(n-1, memo) + climbStairsHelper(n-2, memo);
+            return 1;
 
-        memo[n] = f;
-        return f;
+        int c0 = 1;
+        int c1 = 1;
+        int c = 0;
+
+        for(int i = 2; i <= n; i++) {
+            c = c0 + c1;
+            c0 = c1;
+            c1 = c;
+        }
+
+        return c;
     }
 }
