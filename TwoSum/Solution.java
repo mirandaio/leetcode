b4a3e01c719aa72570820e3e60ca4521d4f5bc6b
Alfredo Miranda <alfredo@miranda.io>
2015-01-10 21:20:16 -0600

    Complete Two Sum

---
+++ TwoSum/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] indexes = new int[2];
+
+        for(int i = 0; i < numbers.length; i++) {
+            if(map.containsKey(target - numbers[i])) {
+                indexes[0] = map.get(target - numbers[i]) + 1;
+                indexes[1] = i + 1;
+                break;
+            } else {
+                map.put(numbers[i], i);
+            }
+        }
+
+        return indexes;
+    }
+}
