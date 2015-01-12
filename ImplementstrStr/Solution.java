7042f204700097848936995d0f1159983e849bba
Alfredo Miranda <alfredo@miranda.io>
2015-01-12 11:21:52 -0600

    Complete Implement strStr without using indexOf method

--- ImplementstrStr/Solution.java
+++ ImplementstrStr/Solution.java
@@ -1,5 +1,22 @@
 public class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        int hLen = haystack.length();
+        int nLen = needle.length();
+        int index = -1;
+
+        for(int i = 0; i <= hLen - nLen; i++) {
+            index = i;
+            for(int j = 0; j < nLen; j++) {
+                if(needle.charAt(j) != haystack.charAt(i + j)) {
+                    index = -1;
+                    break;
+                }
+            }
+
+            if(index != -1)
+                break;
+        }
+
+        return index;
     }
 }
