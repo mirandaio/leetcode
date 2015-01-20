5e5b33fe300dd615831244af3be5fa8772b99c83
Alfredo Miranda <alfredo@miranda.io>
2015-01-19 21:43:46 -0600

    Some improvements and fixes to N-Queens problem

--- NQueens/Solution.java
+++ NQueens/Solution.java
@@ -1,75 +1,53 @@
 public class Solution {
     public List<String[]> solveNQueens(int n) {
-        List<String[]> solutions = new ArrayList<String[]>();
-        char[][] board = new char[n][n];
-
-        for(int row = 0; row < n; row++) {
-            for(int col = 0; col < n; col++)
-                board[row][col] = '.';
-        }
-
-        solveNQueens(solutions, board, 0);
+        ArrayList<String[]> solutions = new ArrayList<String[]>();
+        int[] rows = new int[n];
+        solveNQueens(solutions, rows, 0);
         return solutions;
     }
 
-    private void solveNQueens(ArrayList<String[]> solutions, char[][] board,
+    private void solveNQueens(ArrayList<String[]> solutions, int[] rows,
         int row) {
 
-        if(row >= board.length) {
-            String[] solution = new String[board.length];
-
-            for(int i = 0; i < board.length; i++) {
-                StringBuilder boardRow = new StringBuilder();
-
-                for(int j = 0; j < board.length; j++)
-                    boardRow.append(board[i][j]);
-
-                solution[i] = boardRow.toString();
-            }
-
-            solutions.add(solution);
+        if(row >= rows.length) {
+            solutions.add(buildSolution(rows));
             return;
         }
 
-        for(int col = 0; col < board.length; col++) {
-            board[row][col] = 'Q';
+        for(int col = 0; col < rows.length; col++) {
+            rows[row] = col;
 
-            if(isValid(board, row, col)) {
-                solveNQueens(solutions, board, row + 1);
-            }
-
-            board[row][col] = '.';
+            if(isValid(rows, row))
+                solveNQueens(solutions, rows, row + 1);
         }
     }
 
-    private boolean isValid(char[][] board, int row, int col) {
-        int i;
-        int j;
-        for(i = 0; i < row; i++) {
-            if(board[i][col] == 'Q')
-                return false;
+    private String[] buildSolution(int[] rows) {
+        String[] solution = new String[rows.length];
+
+        for(int i = 0; i < rows.length; i++) {
+            StringBuilder row = new StringBuilder();
+
+            for(int j = 0; j < rows.length; j++) {
+                if(j == rows[i])
+                    row.append('Q');
+                else
+                    row.append('.');
+            }
+
+            solution[i] = row.toString();
         }
 
-        i = row - 1;
-        j = col - 1;
+        return solution;
+    }
 
-        while(i >= 0 && j >= 0) {
-            if(board[i][j] == 'Q')
+    private boolean isValid(int[] rows, int row) {
+        for(int i = 0; i < row; i++) {
+            if(rows[row] == rows[i])
                 return false;
 
-            i--;
-            j--;
-        }
-
-        i = row - 1;
-        j = col + 1;
-
-        while(i >= 0 && j < board.length) {
-            if(board[i][j] == 'Q')
+            if(row - i == Math.abs(rows[row] - rows[i]))
                 return false;
-
-            i--;
-            j++;
         }
 
         return true;
